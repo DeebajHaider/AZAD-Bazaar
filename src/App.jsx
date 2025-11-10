2cc22a4ff3c915d3ec69786ea69bb4093e55b4b8
Deebaj Haider <haiderdeebaj07@gmail.com>
2025-11-10 11:33:18 +0500

    Set up Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to AZAD-Bazaar</h1>
+      <p>A modern, accessible marketplace UI</p>
+    </div>
+  )
+}
+
+export default App
